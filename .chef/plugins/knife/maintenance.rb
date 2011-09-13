509631c2e294211bd95ed2e83877dc32888253bb
Warwick Poole <wpoole@gmail.com>
2011-09-13 07:24:54 -0400

    nginx maintenance mode knife plugin

---
+++ .chef/plugins/knife/maintenance.rb
@@ -0,0 +1,63 @@
+## Knife plugin to enable nginx maintenance mode
+# From Harvest. www.getharvest.com
+#
+# Source: https://github.com/harvesthq/knife-plugins
+#
+# See http://wiki.opscode.com/display/chef/Knife+Plugins
+# And: http://warwickp.com/2011/09/knife-plugin-for-nginx-maintenance-mode
+#
+## Install
+# Place in .chef/plugins/knife/maintenance.rb
+## Configure
+# Set the role name that identifies your target servers in query below
+#
+## Usage
+# $ knife maintenance --enable
+# $ knife maintenance --disable
+
+require 'chef/knife'
+ 
+module Turnoffthedark
+  class Maintenance < Chef::Knife
+ 
+    deps do
+      require 'chef/search/query'
+      require 'chef/knife/ssh'
+      Chef::Knife::Ssh.load_deps
+    end
+    
+    option :enable,
+    :short => '-e',
+    :long => '--enable',
+    :boolean => true,
+    :description => "Turn maintenance mode on! PANIC"
+
+    option :disable,
+    :short => '-d',
+    :long => '--disable',
+    :boolean => true,
+    :description => "Turn maintenance mode off"
+    
+    banner "knife maintenance --enable or --disable"
+    
+    def run
+      
+      if config[:enable]
+        puts "WARNING: You are now turning off the world. PANIC."
+        ssh_command = "sudo ln -s /somewhere/maintenance.html /somewhere/maintenanceON"    
+      elsif config[:disable]
+        puts "OK: Turning us back on"
+        ssh_command = "sudo rm /somewhere/maintenanceON"    
+      else
+        puts "I don't know what you want to do."
+        exit
+      end
+        
+      query = "role:nginx_production_lb"
+      knife_ssh = Chef::Knife::Ssh.new()
+      knife_ssh.name_args = [query, ssh_command]
+      knife_ssh.run
+      
+    end
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# Useful Knife plugins
+
+## Install
+
+Place in directory: $chef_root/.chef/plugins/knife
+
+## See also
+
+- [http://wiki.opscode.com/display/chef/Knife+Plugins](http://wiki.opscode.com/display/chef/Knife+Plugins)
+
+### Credits
+
+- Written by [Harvest](http://www.getharvest.com/)
+- Chef by [Opscode](http://www.opscode.com)
